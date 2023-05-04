29820cf98823a9f85182ccf790c3cdd911d4145e
HarshitKava <harshit.kava@gmail.com>
2023-05-04 10:09:01 +0530

    0

--- LabourReport/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LabourreportConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'LabourReport'
--- static/js/SLI/form.js
+++
@@ -1,5 +0,0 @@
-var n =  new Date();
-var y = n.getFullYear();
-var m = n.getMonth() + 1;
-var d = n.getDate();
-document.getElementById("date").innerHTML = d + "/" + m + "/" + y;
